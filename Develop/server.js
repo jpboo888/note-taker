9430a2ca67d9da69d2288aa1e076d07ffae3a440
JP MALDONADO <jpmaldonado@jps-imac.lan>
2022-08-22 14:54:46 -0500

    fixing site

--- Develop/server.js
+++ Develop/server.js
@@ -1,53 +1,46 @@
-const express = require("express")
-const fs = require("fs")
+const express = require('express');
 const path = require("path")
-const uuid = require("uuid")
-const notes = require("./db/db.json")
+const data = require("./db/db.json")
+const fs = require('fs')
 
-const apiRoutes = require("./routes/apiRoutes")
-const htmlRoutes = require("./routes/htmlRoutes")
+const app = express();
 
-const PORT = process.env.PORT || 3004;
-const app = express()
+// middlewares
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static('public'))
-app.use("/api", apiRoutes)
-app.use("/", htmlRoutes)
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+})
 
-  // API Route | GET
+app.get("/api/notes", (req, res) => {
+    res.json(data)
+})
 
-  app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
+//receives the data from the server and sends it to the db.json
+app.post("/api/notes", (req, res) => {
+    console.log(req.body)
+
+//push the contents of req.body
+    data.push(req.body);
+    fs.writeFile("./db/db.json", JSON.stringify(data), () => {
+        res.send("successfully added!")
     })
 
-  app.get("api/notes", (req, res) =>  {
-    res.sendFile(__dirname, "./db/db.json")
-  })
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"))
-    })
-  
-  app.post("/api/notes", (req, res) =>  {
-    const notes = JSON.parse(fs.readFileSync("./db/db.json"))
-    const newNotes = req.body
-    newNotes.id = uuid.v4()
-    notes.push(newNotes)
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
-    res.json(notes)
-  })
+})
 
 app.delete("/api/notes:id", (req, res) =>  {
-  const notes = JSON.parse(fs.readFileSync("./public/index.html"))
-  const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id)
-  fs.writeFileSync("./db/db.json", JSON.stringify(delNote))
-  res.join(delNote)
-})
- 
+    const notes = JSON.parse(fs.readFileSync("./public/index.html"))
+    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id)
+    fs.writeFileSync("./db/db.json", JSON.stringify(delNote))
+    res.join(delNote)
+  })
 
-    app.listen(PORT, () => {
-      console.log(`API server now on port ${PORT}!`);
-    });
+app.listen(3001, () => {
+    console.log("Server is running")
+})
